fbeb269ebfe41b95e733b4394ce317ca4a99e6d2
Ayrat <112854982+KabAyrat@users.noreply.github.com>
2024-09-05 22:31:37 +0300

    Update program.cs

--- Лаба_1/Вариант_5/Задание_1/Пример_2/program.cs
+++ Лаба_1/Вариант_5/Задание_1/Пример_2/program.cs
@@ -1,1 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите a: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+        double z = Math.Pow(Math.Cos(a), 2) + Math.Pow(Math.Cos(a), 4);
+        Console.WriteLine(z);
+    }
+}
 
