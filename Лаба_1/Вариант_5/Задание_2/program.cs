3370706dfc0f4a935f4afa96e2951117ce5a9624
Ayrat <112854982+KabAyrat@users.noreply.github.com>
2024-09-05 22:36:15 +0300

    Update program.cs

--- Лаба_1/Вариант_5/Задание_2/program.cs
+++ Лаба_1/Вариант_5/Задание_2/program.cs
@@ -1,1 +1,29 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение a: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите значение b: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите значение x: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        double y;
+
+        if (Math.Abs(a - Math.Pow(b, 2)) > b)
+        {
+            y = Math.Log(Math.Abs(a * x - b)) - Math.Exp(Math.Atan(x));
+        }
+        else
+        {
+            y = Math.Tan(4 * x - a);
+        }
+        Console.WriteLine(y);
+    }
+}
 
