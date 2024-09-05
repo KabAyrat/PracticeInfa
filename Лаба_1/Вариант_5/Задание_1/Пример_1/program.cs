473ee1d190126435b741f8b4e3f70a73acda2202
Ayrat <112854982+KabAyrat@users.noreply.github.com>
2024-09-05 22:29:58 +0300

    Update program.cs

--- Лаба_1/Вариант_5/Задание_1/Пример_1/program.cs
+++ Лаба_1/Вариант_5/Задание_1/Пример_1/program.cs
@@ -1,1 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+        {
+        Console.Write("Введите a: ");
+        double num = Convert.ToDouble(Console.ReadLine());
+        double z = 1 - (1.0 / 4) * Math.Pow(Math.Sin(2 * num), 2) + Math.Cos(2 * num);
+        Console.WriteLine(z);
+    }
+}
 
