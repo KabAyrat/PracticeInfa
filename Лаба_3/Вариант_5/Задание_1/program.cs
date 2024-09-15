a7dc8b5aa7df03ec1df0521a2b29a28589264ad3
Ayrat <112854982+KabAyrat@users.noreply.github.com>
2024-09-15 16:31:53 +0300

    Update program.cs

--- Лаба_3/Вариант_5/Задание_1/program.cs
+++ Лаба_3/Вариант_5/Задание_1/program.cs
@@ -5,45 +5,35 @@ class Program
 {
     static void Main()
     {
-        // Пример одномерного массива
+
         int[] arr = { 3, -2, 5, 8, -7, 6, -4, 1 };
 
-        // Вывод массива на экран
         Console.WriteLine("Массив: " + string.Join(", ", arr));
 
-        // 1. Поиск максимального по модулю элемента
-        int maxAbs = arr.OrderByDescending(Math.Abs).First();
-        Console.WriteLine("Максимальный по модулю элемент: " + maxAbs);
+        int maxModul = arr.OrderByDescending(Math.Abs).First();
+        Console.WriteLine("Максимальный по модулю элемент: " + maxModul);
 
-        // 2. Поиск первого и второго отрицательных элементов
-        int firstNegativeIndex = Array.FindIndex(arr, x => x < 0);
-        int secondNegativeIndex = Array.FindIndex(arr, firstNegativeIndex + 1, x => x < 0);
+        // Поиск первого и второго минусового числа
+        int firstNegativeChislo = Array.FindIndex(arr, x => x < 0);
+        int secondNegativeChislo = Array.FindIndex(arr, firstNegativeChislo + 1, x => x < 0);
 
-        if (firstNegativeIndex != -1 && secondNegativeIndex != -1)
-        {
-            // 3. Вычисление среднего арифметического между первым и вторым отрицательными элементами
+        if (firstNegativeChislo != -1 && secondNegativeChislo != -1){
             int sum = 0;
             int count = 0;
 
-            for (int i = firstNegativeIndex + 1; i < secondNegativeIndex; i++)
-            {
+            for (int i = firstNegativeChislo + 1; i < secondNegativeChislo; i++){
                 sum += arr[i];
                 count++;
             }
 
-            if (count > 0)
-            {
+            if (count > 0){
                 double avg = (double)sum / count;
-                Console.WriteLine("Среднее арифметическое элементов между первым и вторым отрицательными элементами: " + avg);
+                Console.WriteLine("Среднее арифметическое: " + avg);
+            }else{
+                Console.WriteLine("таких элементов нет .");
             }
-            else
-            {
-                Console.WriteLine("Между первым и вторым отрицательными элементами нет элементов.");
-            }
-        }
-        else
-        {
-            Console.WriteLine("Недостаточно отрицательных элементов для выполнения вычислений.");
+        }else{
+            Console.WriteLine("Недостаточно отрицательных элементов");
         }
     }
 }
