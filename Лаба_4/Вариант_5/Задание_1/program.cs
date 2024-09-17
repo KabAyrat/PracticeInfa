1602da3c2765423c5629de3d3a6b193b914b51d8
Ayrat <112854982+KabAyrat@users.noreply.github.com>
2024-09-17 11:10:45 +0300

    Update program.cs

--- Лаба_4/Вариант_5/Задание_1/program.cs
+++ Лаба_4/Вариант_5/Задание_1/program.cs
@@ -82,3 +82,76 @@ class Program
     }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        /* List<string> names = ["<name>", "Ana", "Felipe"];
+         Console.WriteLine();
+         names.Add("Maria");
+         names.Add("Bill");
+         names.Remove("Ana");
+         foreach (var name in names)
+         {
+             Console.WriteLine($"Hello {name.ToUpper()}!");
+         }*/
+
+        List<int> nums = new List<int>(5) {1,2,3,4,5};
+        Console.WriteLine("Начальный список:");             //      1
+        foreach (int x in nums)
+        {
+            Console.WriteLine(x);
+        }
+        nums.Add(6);
+
+
+        Console.WriteLine("Добавили элемент в конец списка:");         
+        foreach (int x in nums)
+        {
+            Console.WriteLine(x);
+        }
+
+
+        List<int> nums2 = new List<int>(3) {8, 9, 10};           //      2
+        Console.WriteLine("Второй список:"); 
+        foreach (int x in nums2)
+        {
+            Console.WriteLine(x);
+        }
+
+
+        nums.InsertRange(2, nums2);
+        Console.WriteLine("первый список после добавдения второго в него:");            //  3
+        foreach (int x in nums)
+        {
+            Console.WriteLine(x);
+        }
+        Console.WriteLine("Длина всего первого списка: "+ nums.Count) ;               //      4
+
+
+        Console.WriteLine("Максимальное значение списка: "+ nums.Max());            //      5
+        Console.WriteLine("Минимальное значение списка: " + nums.Min());            //      6
+
+
+        foreach (int item in nums2)
+        {
+            Console.Write(item + " ");
+        }
+
+    }
+}
