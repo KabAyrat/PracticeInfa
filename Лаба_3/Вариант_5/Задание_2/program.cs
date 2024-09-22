67694e39d976615866f64ab27ac1e80df8571181
Ayrat <112854982+KabAyrat@users.noreply.github.com>
2024-09-22 16:31:44 +0300

    Update program.cs

--- Лаба_3/Вариант_5/Задание_2/program.cs
+++ Лаба_3/Вариант_5/Задание_2/program.cs
@@ -1,1 +1,70 @@
+using System;
 
+class Program
+{
+    static void Main()
+    {
+        // Создаем целочисленную квадратную матрицу
+        int[,] matrix = {
+            { 2, 4, 6 },
+            { 7, 8, 10 },
+            { 12, 14, 16 }
+        };
+
+        int size = matrix.GetLength(0);
+
+        Console.WriteLine("Исходная матрица:");
+        PrintMatrix(matrix);
+
+        // Находим и выводим номера строк, состоящих только из четных элементов
+        Console.WriteLine("\nНомера строк, состоящих только из четных элементов:");
+        for (int i = 0; i < size; i++)
+        {
+            bool allEven = true;
+            for (int j = 0; j < size; j++)
+            {
+                if (matrix[i, j] % 2 != 0)
+                {
+                    allEven = false;
+                    break;
+                }
+            }
+            if (allEven)
+            {
+                Console.WriteLine($"Строка {i + 1}");
+            }
+        }
+
+        // Меняем местами 1 и 3 строки матрицы
+        SwapRows(matrix, 0, 2);
+
+        Console.WriteLine("\nМатрица после замены 1 и 3 строк:");
+        PrintMatrix(matrix);
+    }
+
+    // Метод для вывода матрицы
+    static void PrintMatrix(int[,] matrix)
+    {
+        int size = matrix.GetLength(0);
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                Console.Write(matrix[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    // Метод для замены строк матрицы
+    static void SwapRows(int[,] matrix, int row1, int row3)
+    {
+        int size = matrix.GetLength(1);
+        for (int j = 0; j < size; j++)
+        {
+            int temp = matrix[row1, j];
+            matrix[row1, j] = matrix[row3, j];
+            matrix[row3, j] = temp;
+        }
+    }
+}
