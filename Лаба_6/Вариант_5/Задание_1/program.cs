fb820304b1d755af5767f7806623517f98f74d06
Ayrat <112854982+KabAyrat@users.noreply.github.com>
2024-09-24 10:34:04 +0300

    Update program.cs

--- Лаба_6/Вариант_5/Задание_1/program.cs
+++ Лаба_6/Вариант_5/Задание_1/program.cs
@@ -1,1 +1,16 @@
+using System;
+using System.Linq;
+using System.IO;
 
+class Program
+{
+    static void Main()
+    {
+        string file = "test.txt";
+        string reader = File.ReadAllText(file);
+
+        char[] letters = { 'A', 'a', 'O', 'o', 'I', 'i', 'Y', 'y', 'E', 'e', 'U', 'u' };
+
+        string[] words = reader.Split(letters[] {' ',',','.'}, StringSplitOptions.RemoveEmptyEntries);
+    }
+}
