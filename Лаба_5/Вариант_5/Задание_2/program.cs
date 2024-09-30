cf5ca3723ca9f37ef7e7460411d2563db50650a7
Ayrat <112854982+KabAyrat@users.noreply.github.com>
2024-09-30 22:32:46 +0300

    Update program.cs

--- Лаба_5/Вариант_5/Задание_2/program.cs
+++ Лаба_5/Вариант_5/Задание_2/program.cs
@@ -5,24 +5,74 @@ class Program
 {
     static void Main()
     {
+        //Console.WriteLine("Введите текст:");
+        //string input = Console.ReadLine();
+
+        //string shiftedText = new string(input.Select(c =>
+        //{
+        //    if (char.IsLetter(c))
+        //    {
+        //        // Если это строчная буква
+        //        if (char.IsLower(c))
+        //        {
+        //            if (c == 'z') 
+        //            {
+        //                return 'a';
+        //            }
+        //            else
+        //            {
+        //                return (char)(c + 1);
+        //            }
+        //        }
+        //        else if (char.IsUpper(c))
+        //        {
+        //            if (c == 'Z') 
+        //            {
+        //                return 'A';
+        //            }
+        //            else
+        //            {
+        //                return (char)(c + 1);
+        //            }
+        //        }
+        //    }
+        //    return c;
+        //}).ToArray());
+
+        //Console.WriteLine($"Сдвинутый текст: {shiftedText}");
+
         Console.WriteLine("Введите текст, заканчивающийся точкой:");
-        string input = Console.ReadLine();
-        string textWithoutDot = input.TrimEnd('.');
-        string[] words = textWithoutDot.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
-        int maxWordLength = words.Max(word => word.Count(char.IsLetter));
+        string input = Console.ReadLine().TrimEnd('.'); 
+
+        string[] words = input.Split(' ', ',');
+
+        int maxWordLength = words.Where(word => word != "").Max(word => word.Length);
+
         Console.WriteLine($"K = {maxWordLength}");
-        char ShiftChar(char c, int shift)
-        {
-            if (char.IsLetter(c))
-            {
-                char offset = char.IsUpper(c) ? 'A' : 'a';
-                return (char)((((c - offset) + shift) % 26) + offset);
+
+        string secretText = new string(input.Select(c => {
+            if (char.IsLetter(c)){
+                char upperLimit;
+                char lowerLimit;
+
+                if (char.IsUpper(c)){
+                    upperLimit = 'Z';
+                    lowerLimit = 'A';
+                } else{
+                    upperLimit = 'z';
+                    lowerLimit = 'a';
+                }
+
+                if (c + maxWordLength > upperLimit){
+                    return (char)(c + maxWordLength - 26);
+                } else{
+                    return (char)(c + maxWordLength);
+                }
+            } else{
+                return c;
             }
-            return c; 
-        }
+        }).ToArray());
 
-        string encryptedText = new string(input.Select(c => ShiftChar(c, maxWordLength)).ToArray());
-
-        Console.WriteLine($"Зашифрованный текст: {encryptedText}");
+        Console.WriteLine($"Зашифрованный текст: {secretText}");
     }
 }
