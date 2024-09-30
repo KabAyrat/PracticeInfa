6d485f6eb2d5802c976fdb32af3f7b99234e7a39
Ayrat <112854982+KabAyrat@users.noreply.github.com>
2024-09-30 22:17:52 +0300

    Update program.cs

--- Лаба_5/Вариант_5/Задание_1/program.cs
+++ Лаба_5/Вариант_5/Задание_1/program.cs
@@ -2,33 +2,24 @@ using System;
 
 class Program
 {
-    static string ReverseWords(string input)
-    {
-        string[] words = input.Split(' ');
-        Array.Reverse(words);
-        return string.Join(" ", words);
-    }
-
-    static string ReverseString(string input)
-    {
-        char[] charArray = input.ToCharArray();
-        Array.Reverse(charArray);
-        return new string(charArray);
-    }
     static void Main()
     {
-
         Console.WriteLine("Введите что-нибудь:");
         string input = Console.ReadLine();
 
-        string reversedWords = ReverseWords(input);
+        string[] words = input.Split(' ');
 
-        Console.WriteLine("словами наоборот: ");
+        Array.Reverse(words);
+        string reversedWords = string.Join(" ", words);
+
+        Console.WriteLine("Слова задом наперед:");
         Console.WriteLine(reversedWords);
 
-        string reversedString = ReverseString(reversedWords);
-        Console.WriteLine("Строка в обратном порядке:");
+        char[] charArray = input.ToCharArray();
+        Array.Reverse(charArray);
+        string reversedString = new string(charArray);
+
+        Console.WriteLine("Текст задом наперед:");
         Console.WriteLine(reversedString);
     }
-
 }
